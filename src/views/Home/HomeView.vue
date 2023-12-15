56d6bbffd856d9dd2230ef0a4e56a9de75d4e47d
hikaru1996 <mzzt1996@outlook.com>
2023-12-15 21:24:34 +0800

    12.15

---
+++ src/views/Home/HomeView.vue
@@ -0,0 +1,20 @@
+<script setup>
+
+</script>
+
+<template>
+  <main>
+    <!-- 速记和跳转发布笔记栏 -->
+    <div class="">
+      
+    </div>
+    <!-- 最近状态统计 -->
+    <div>
+
+    </div>
+    <!-- 大事记列表 -->
+    <div>
+
+    </div>
+  </main>
+</template>
